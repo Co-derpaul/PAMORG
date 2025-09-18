cb80c16ba6a8c6cb523a1be76dd720d615e2f313
Paul_Ameh <152933737+Co-derpaul@users.noreply.github.com>
2025-09-18 03:03:20 +0100

    Add files via upload

---
+++ BLOG/Contact.js
@@ -0,0 +1,18 @@
+
+ 
+// setTimeout(myFunction, 3000);
+// function myFunction() {
+//     alert('Hello\nWelcome to PAMORG');
+// }
+
+// CharacterCount
+function characterCount(){
+    var element=document.getElementById('feedback').value.length;
+   document.getElementById('textarea_count').innerHTML= element+ "/500 (Max Character 500).";
+
+  }
+  characterCount()
+  //   console.log(characterCount())
+
+//   EmailJS
+
